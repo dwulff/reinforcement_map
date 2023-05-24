b2808ae35b2d34dd1cc366ee954ad01e3f4e7d4b
bolenz <bolenz@mpib-berlin.mpg.de>
2023-05-24 10:38:16 +0200

    changed to tidytext tokenizer

--- 2_code/4_word_frequencies.R
+++ 2_code/4_word_frequencies.R
@@ -1,25 +1,33 @@
-require(tm)
+# this script compares the frequency of words in the abstracts against English wikipedia
+
+require(tidyverse)
+library(tidytext)
+
+data <- read.csv("1_data/data_cleaned.csv")
+
 
 # tokenize abstracts
-abstracts_tokens <- tm::Boost_tokenizer(data$Abstract_cleaned)
+abstracts_df <- tibble(abstract=1:nrow(data), text=data$Abstract_cleaned)
+
+abstracts_tokenized <- abstracts_df %>% unnest_tokens(word, text)
 
 # count unique tokens
-abstracts_tokens <- abstracts_tokens %>%
+abstracts_tokens <- abstracts_tokenized %>%
   as_tibble() %>%
-  group_by(value) %>%
+  group_by(word) %>%
   summarize(n = n()) 
 
 
 # how many tokens have a number or a digit in it?
-mean(str_detect(abstracts_tokens$value, "[:punct:]|[:digit:]"))
+mean(str_detect(abstracts_tokens$word, "[:punct:]|[:digit:]"))
 
 
 # read wikipedia data
-wiki <- read_delim("1_data/wiki_word_frequency/enwiki-2023-04-13.txt", col_names = c("token", "n_wiki"))
+wiki <- read_delim("1_data/wiki_word_frequency/enwiki-2023-04-13.txt", col_names = c("word", "n_wiki"))
 
 # combine abstract data with wikipedia data. only words that occur at least once in each data set will be considered
-df <- abstracts_tokens %>%
-  inner_join(wiki, by=join_by(value==token)) %>%
+tokens <- abstracts_tokens %>%
+  inner_join(wiki, by=join_by(word)) %>%
   rename(n_abstracts = n) %>%
   mutate(logp_abstracts = log(n_abstracts/sum(n_abstracts)),
          logp_wiki = log(n_wiki/sum(n_wiki)),
@@ -27,4 +35,4 @@ df <- abstracts_tokens %>%
 
 
 # sort by logp_diff descending 
-df %>% arrange(-logp_diff) %>% print(n = 20)
+tokens %>% dplyr::arrange(-logp_diff) %>% print(n = 20)
